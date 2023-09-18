bd73da21d1f47a3b737b0c6993e0f21e67a018ca
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-09-18 12:09:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "equal to Chelsea = (Chelsea)";
+
+mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "equal to Susan Smith = (Susan Smith)";
+
+companyCeo = "Susan Smith";
